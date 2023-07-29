2197b317b1c938191ecee8d42bba2791afd8ad5c
Vyacheslav <dikey677@gmail.com>
2023-07-29 22:02:23 +0300

    Update Timer.jsx

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,29 +1,57 @@
 import React, { useState, useEffect } from "react";
 
 const Timer = () => {
-  const [initialValue, setInitialValue] = useState(0);
+  const [initialSeconds, setInitialSeconds] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [over, setOver] = React.useState(false);
 
-  useEffect(() => {});
+  useEffect(() => {
+    const timerID = setInterval(() => runTimer(), 1000);
+    return () => clearInterval(timerID);
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValue);
+  useEffect(() => {
+    //Calc hours
+    setHours(Math.floor(initialSeconds / 3600));
+
+    //Calc mins
+    setMinutes(Math.floor(initialSeconds / 60) - hours * 60);
+
+    //Calc secs
+    setSeconds(initialSeconds % 60);
+    console.log("Total hours", hours, "minutes", minutes, "seconds", seconds);
+  });
+
+  const runTimer = () => {
+    // if (over) return;
+
+    setSeconds((prev) => prev - 1);
+
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+      setOver(true);
+    } else if (minutes === 0 && seconds === 0) {
+      setHours((prev) => prev - 1);
+      setMinutes(59);
+      setSeconds(59);
+    } else if (seconds === 0) {
+      setMinutes((prev) => prev - 1);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <div>
         <input
           type="number"
           placeholder="Seconds"
-          value={initialValue}
-          onChange={(e) => setInitialValue(e.currentTarget.value)}
+          value={initialSeconds}
+          onChange={(e) => setInitialSeconds(e.target.value)}
         />
-        <button type="Submit">Start</button>
-      </form>
+
+        <button type="button">Start</button>
+      </div>
 
       <span>
         {hours < 10 ? "0" + hours : hours}:
