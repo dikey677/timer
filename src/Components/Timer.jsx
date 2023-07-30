570eabe6fb8f6c38ccf998bdc477df0c27729b76
Vyacheslav <dikey677@gmail.com>
2023-07-30 22:13:56 +0300

    Update Timer.jsx

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -5,40 +5,48 @@ const Timer = () => {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [over, setOver] = React.useState(false);
+  const [overtime, setOvertime] = React.useState(false);
 
-  useEffect(() => {
-    const timerID = setInterval(() => runTimer(), 1000);
-    return () => clearInterval(timerID);
-  });
-
-  useEffect(() => {
+  const calculateTime = () => {
     //Calc hours
     setHours(Math.floor(initialSeconds / 3600));
 
     //Calc mins
-    setMinutes(Math.floor(initialSeconds / 60) - hours * 60);
+    setMinutes(Math.floor((initialSeconds / 60) % 60));
 
     //Calc secs
     setSeconds(initialSeconds % 60);
-    console.log("Total hours", hours, "minutes", minutes, "seconds", seconds);
-  });
+  };
 
   const runTimer = () => {
-    // if (over) return;
-
-    setSeconds((prev) => prev - 1);
+    // if (overtime) return;
 
     if (hours === 0 && minutes === 0 && seconds === 0) {
-      setOver(true);
-    } else if (minutes === 0 && seconds === 0) {
+      return;
+    } else if (minutes === 0) {
       setHours((prev) => prev - 1);
       setMinutes(59);
-      setSeconds(59);
     } else if (seconds === 0) {
       setMinutes((prev) => prev - 1);
+      setSeconds(59);
+    } else {
+      setSeconds((prev) => prev - 1);
     }
   };
+
+  const reset = () => {
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+    setOvertime(false);
+  };
+
+  useEffect(() => {
+    console.log(`Total hours ${hours} minutes ${minutes} seconds ${seconds}`);
+    const timerID = setInterval(() => runTimer(), 10);
+
+    return () => clearInterval(timerID);
+  });
 
   return (
     <div>
@@ -50,7 +58,9 @@ const Timer = () => {
           onChange={(e) => setInitialSeconds(e.target.value)}
         />
 
-        <button type="button">Start</button>
+        <button type="button" onClick={calculateTime}>
+          Start
+        </button>
       </div>
 
       <span>
